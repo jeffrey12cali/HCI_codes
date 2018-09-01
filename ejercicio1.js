9c1282dfffd4e321701a4d481bbd685efcf20ecd
goeff <jeffrey12cali@javerianacali.edu.co>
2018-09-01 08:48:29 -0500

    Ejercicio terminado

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,18 +4,19 @@ function años(edad){
 }
 
 function a_texto(nueva_edad){
-	return string(nueva_edad)
+	return nueva_edad.toString();
 }
 
 function ejercicio(){
-	var edad = prompt("Ingrese su edad: ");
+	var edad = Number(prompt("Ingrese su edad: "));
 	//var años = (edad) => edad*365;
 	var nueva_edad = años(edad);
 	if (nueva_edad % 2 == 0){
 		var texto = a_texto(nueva_edad);
+		console.log(texto);
 	}
 	else{
-
+		var array = [edad, nueva_edad];
+		console.log(array);
 	}
-	console.log(nueva_edad);
 }
