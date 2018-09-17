19fd987a2a07a7854cbfee8985788ecc121cbfe3
Geoff <jeffrey12cali@javerianacali.edu.co>
2018-09-17 17:28:37 -0500

    quiz

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+function años(edad){
+	var aux = edad * 365;
+	return aux;
+}
+
+function a_texto(nueva_edad){
+	return nueva_edad.toString();
+}
+
+function ejercicio(){
+	var edad = Number(prompt("Ingrese su edad: ");
+	//var años = (edad) => edad*365;
+	var nueva_edad = años(edad);
+	if (nueva_edad % 2 == 0){
+		var texto = a_texto(nueva_edad);
+		console.log(texto);
+	}
+	else{
+		var array = [edad, nueva_edad];
+		console.log(array);
+	}
+}
