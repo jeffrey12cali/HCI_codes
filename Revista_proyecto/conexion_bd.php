40fa9bce7f47a1d867eb325ce4545892e4b23f98
root <jeffrey12cali@javerianacali.edu.co>
2018-10-04 17:52:43 -0500

    Revista proyecto

---
+++ Revista_proyecto/conexion_bd.php
@@ -0,0 +1,10 @@
+<?php
+	$host = 'localhost';
+	$user = 'root';
+	$pw = '';
+	$bd = '';
+	$mysqli = new mysqli('localhost', 'root', '', 'mi_revista');
+	$mysqli -> set_charset("utf8");
+	$queryCiudad = $mysqli -> query("SELECT * FROM ciudad");
+	$queryRevista = $mysqli -> query("SELECT * FROM revista");
+?>
