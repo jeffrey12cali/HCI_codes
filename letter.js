f7fa07049921438da27955e3f32b04c80f66f00d
Geoff <jeffrey12cali@javerianacali.edu.co>
2018-10-23 22:47:24 -0500

    Ejercicio letters

--- letter.js
+++ letter.js
@@ -1,17 +1,11 @@
-function solve(s, k){
-    let j = 0;
-    let flag = false;
-    let ascii = 97;
-    while (j < k){
-        flag = false;
-        for (let i = 0; i < s.length; i++){
-            if (ascii == s.charCodeAt(i))
-                flag = true;
-                s = s.replace(s[i],'');
-        }
-        if (flag == true)
-            j += 1;
-        ascii += 1;
-    }
-    return s;
+function solve(s,k){
+	let letters = "abcdefghijklmnopqrstuvwxyz"
+	let ersd = 0
+	for (let i=0; i < letters.length && ersd < k; i++){
+		while (s.includes(letters[i]) && ersd < k){
+			s = s.replace(letters[i], '')
+			ersd +=1
+		}
+	}
+	return s
 }
